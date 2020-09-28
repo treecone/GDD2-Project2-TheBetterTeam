d6b82855a04f5d37b0db66184ef08648557a0bad
treecone <zartsky8@gmail.com>
2020-09-28 19:00:18 -0400

    First commit as well as early physics and frame movement

---
+++ GDD2-Project2Game-TheBetterTeam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> moveableObjects;
+
+    void Start()
+    {
+        GameObject[] allObj = FindObjectsOfType<GameObject>();
+        foreach(GameObject moveObj in allObj)
+        {
+            if(moveObj.GetComponent<MoveableObject>() != null)
+            {
+                moveableObjects.Add(moveObj);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debugging
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ForwardTime();
+        }
+    }
+
+    public void ForwardTime ()
+    {
+        foreach(GameObject obj in moveableObjects)
+        {
+            //Gravity
+            obj.GetComponent<MoveableObject>().ApplyTime(Vector2.down);
+        }
+    }
+}
---
+++ GDD2-Project2Game-TheBetterTeam/Assets/Scripts/MoveableObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObject : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    IEnumerator Move (Vector2 direction, float deltaTime)
+    {
+        Vector2 endPosition = (Vector2)transform.position + direction;
+        float newTime = Time.time;
+        while (Time.time < newTime + deltaTime)
+        {
+            transform.position = Vector2.Lerp(transform.position, endPosition, (Time.time - newTime) / deltaTime);
+            yield return null;
+        }
+        transform.position = endPosition;
+    }
+
+    public void ApplyTime (Vector2 direction)
+    {
+
+        //Movement
+        if(direction != Vector2.zero)
+        {
+            if(!CheckCollision(direction))
+            {
+                StartCoroutine(Move(direction, 0.1f));
+            }
+        }
+    }
+
+    public bool CheckCollision (Vector2 dir)
+    {
+        Debug.DrawRay((Vector2)transform.position + (dir * 1.05f), dir * 0.45f, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position + dir*1.05f, dir, 0.45f);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        return false;
+    }
+}
