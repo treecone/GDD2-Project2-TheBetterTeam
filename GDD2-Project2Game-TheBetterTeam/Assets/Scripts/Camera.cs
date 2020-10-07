b72d7122c9202813dc397f4a35e701579649c80c
treecone <zartsky8@gmail.com>
2020-10-07 17:41:45 -0400

    UI and Camera
    Added TMPRO, make a rough main menu and added the script that controls the camera to move to the player.

---
+++ GDD2-Project2Game-TheBetterTeam/Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private GameObject target;
+
+    private void Start()
+    {
+        if (target == null)
+            target = GameObject.FindWithTag("Player");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Slerp(transform.position, target.transform.position + offset, moveSpeed * Time.deltaTime);
+    }
+}
