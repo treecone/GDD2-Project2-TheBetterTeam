18d2dbdee46ece81b06fee63f0c1405067bee3d6
Emily Horton <emilyhorton1994@gmail.com>
2020-10-19 23:55:39 -0400

    Add script to playercanvas to hide on load

---
+++ GDD2-Project2Game-TheBetterTeam/Assets/Scripts/HideMenuOnStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMenuOnStart : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject menu;
+
+    private void Awake()
+    {
+        menu.SetActive(false);
+    }
+}
