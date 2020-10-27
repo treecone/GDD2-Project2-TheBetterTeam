2c7eac479819a87b26258122d6bc4758cd0ecd29
RyanTytka <rdt7867@rit.edu>
2020-10-27 16:17:35 -0400

    updated new metor sprite

--- GDD2-Project2Game-TheBetterTeam/Assets/Scripts/Canvas/CanvasMeteor.cs
+++ GDD2-Project2Game-TheBetterTeam/Assets/Scripts/Canvas/CanvasMeteor.cs
@@ -9,9 +9,16 @@ public class CanvasMeteor : MonoBehaviour
 
     public void SetCollectionStatus(bool isCollected)
     {
+        Vector3 v = gameObject.GetComponent<RectTransform>().localPosition;
         if (isCollected)
+        {
+            gameObject.GetComponent<RectTransform>().localPosition = new Vector3(v.x, 152, 0);
             gameObject.GetComponent<Image>().sprite = collected;
+        }
         else
+        {
+            gameObject.GetComponent<RectTransform>().localPosition = new Vector3(v.x, 127, 0);
             gameObject.GetComponent<Image>().sprite = empty;
+        }
     }
 }
