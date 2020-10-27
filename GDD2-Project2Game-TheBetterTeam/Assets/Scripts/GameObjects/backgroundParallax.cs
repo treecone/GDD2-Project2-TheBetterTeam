255bbb2b51dfcb9f6015b4657d3a74a25b0adb77
RyanTytka <rdt7867@rit.edu>
2020-10-27 19:36:18 -0400

    added background parrallax

---
+++ GDD2-Project2Game-TheBetterTeam/Assets/Scripts/GameObjects/backgroundParallax.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundParallax : MonoBehaviour
+{
+    public GameObject camera;
+    public int offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3((camera.transform.position.x * -.1f) + offset, transform.position.y, transform.position.z);
+    }
+}
